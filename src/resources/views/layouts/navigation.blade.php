0672844189370569a97651095b01034a54329532
Josh Espinoza <joshespi@gmail.com>
2026-04-05 01:37:24 +0000

    cleanup,  icon change

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -3,8 +3,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('dashboard') }}" class="flex items-center gap-2">
+                        <x-application-logo class="block h-8 w-auto fill-current text-indigo-600" />
+                        <span class="font-semibold text-gray-800 text-sm hidden sm:block">{{ config('app.name') }}</span>
                     </a>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
