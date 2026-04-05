5bc51c455dafc6f697900d467f39152ad30ca509
Josh Espinoza <joshespi@gmail.com>
2026-04-05 03:02:08 +0000

    Fix trustProxies to use CIDR notation instead of wildcard

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('portfolios:snapshot')->dailyAt('00:05');
     })
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: '0.0.0.0/0');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
