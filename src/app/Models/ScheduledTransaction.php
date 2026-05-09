b95ff375cfa3b2eba8cc7d408d96fb2a0af4f4aa
Josh Espinoza <joshespi@gmail.com>
2026-05-09 19:41:10 +0000

    Scheduled Transactions

--- src/app/Models/ScheduledTransaction.php
+++ src/app/Models/ScheduledTransaction.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ScheduledTransaction extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'user_id', 'description', 'amount', 'type', 'recurrence',
         'next_due_at', 'envelope_id', 'cash_account_id', 'is_active',
